201dc769983149573b15ef7124a2d47c82fca570
Zbigniew Serafin <92160264+zbysiukaczucha@users.noreply.github.com>
2024-12-30 12:07:37 +0100

    Delete GnomeCunning/Saved directory

--- GnomeCunning/Saved/ShaderDebugInfo/PCD3D_SM6/M_Master_Rotation_53cf6a1ae50766fa/Default/FLocalVertexFactory/FDebugViewModeVS/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="D:\Program Files\UE_5.4\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "D:\Program Files\UE_5.4\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% -HV 2018 -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T vs_6_6 -E Main -Fc DebugViewModeVertexShader.d3dasm -Fo DebugViewModeVertexShader.dxil DebugViewModeVertexShader.usf
-:END
-PAUSE
--- GnomeCunning/Saved/ShaderDebugInfo/PCD3D_SM6/M_Master_Rotation_53cf6a1ae50766fa/Default/FLocalVertexFactory/FRayTracingDynamicGeometryConverterCS/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="D:\Program Files\UE_5.4\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "D:\Program Files\UE_5.4\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% -HV 2018 -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T cs_6_6 -E RayTracingDynamicGeometryConverterCS -Fc RayTracingDynamicMesh.d3dasm -Fo RayTracingDynamicMesh.dxil RayTracingDynamicMesh.usf
-:END
-PAUSE
